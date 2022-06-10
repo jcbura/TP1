745436f88cfae5921d493291ed01eaf2a161e66d
jcbura <73317300+jcbura@users.noreply.github.com>
2022-06-10 16:01:46 -0500

    Update py_cron_prog1.py

--- py_cron_prog1.py
+++ py_cron_prog1.py
@@ -1,5 +1,5 @@
 from datetime import datetime
 now = datetime.now()
-f = open("/Users/jason/PERSONAL_COMP/python_cron_test/cron_results.txt", "a")
+f = open("", "a")
 f.write("Adding Line from program python_cron_program_1 @ {}\n".format(now))
 f.close()
